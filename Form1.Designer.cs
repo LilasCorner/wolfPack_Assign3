39998ec6ef35ed18643f8cff716093a13ee7ccb6
Lilo <59840908+LilasCorner@users.noreply.github.com>
2021-09-28 18:06:02 -0500

    award section complete

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -53,8 +53,20 @@ namespace wolfPack_Assign3
             this.label9 = new System.Windows.Forms.Label();
             this.subComboBox = new System.Windows.Forms.ComboBox();
             this.userComboBox = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lessThanRadioButton = new System.Windows.Forms.RadioButton();
+            this.greaterThanRadioButton = new System.Windows.Forms.RadioButton();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.dateQuery = new System.Windows.Forms.Button();
+            this.subQuery = new System.Windows.Forms.Button();
+            this.userQuery = new System.Windows.Forms.Button();
+            this.awardQuery = new System.Windows.Forms.Button();
+            this.userPostQuery = new System.Windows.Forms.Button();
+            this.thresholdQuery = new System.Windows.Forms.Button();
             this.subPanel.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -84,7 +96,7 @@ namespace wolfPack_Assign3
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label3.Location = new System.Drawing.Point(15, 229);
+            this.label3.Location = new System.Drawing.Point(15, 217);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(170, 24);
             this.label3.TabIndex = 2;
@@ -95,7 +107,7 @@ namespace wolfPack_Assign3
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label4.Location = new System.Drawing.Point(15, 375);
+            this.label4.Location = new System.Drawing.Point(15, 341);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(283, 24);
             this.label4.TabIndex = 3;
@@ -106,7 +118,7 @@ namespace wolfPack_Assign3
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label5.Location = new System.Drawing.Point(15, 495);
+            this.label5.Location = new System.Drawing.Point(15, 482);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(329, 24);
             this.label5.TabIndex = 4;
@@ -117,7 +129,7 @@ namespace wolfPack_Assign3
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label6.Location = new System.Drawing.Point(15, 615);
+            this.label6.Location = new System.Drawing.Point(15, 599);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(325, 24);
             this.label6.TabIndex = 5;
@@ -128,7 +140,7 @@ namespace wolfPack_Assign3
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.86792F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label7.Location = new System.Drawing.Point(431, 15);
+            this.label7.Location = new System.Drawing.Point(438, 15);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(71, 24);
             this.label7.TabIndex = 6;
@@ -136,11 +148,11 @@ namespace wolfPack_Assign3
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(435, 49);
+            this.textBox1.Location = new System.Drawing.Point(442, 49);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(509, 658);
+            this.textBox1.Size = new System.Drawing.Size(582, 668);
             this.textBox1.TabIndex = 7;
             // 
             // specificDatePicker
@@ -156,7 +168,7 @@ namespace wolfPack_Assign3
             this.subPanel.Controls.Add(this.avgSub);
             this.subPanel.Controls.Add(this.highSub);
             this.subPanel.Controls.Add(this.lowSub);
-            this.subPanel.Location = new System.Drawing.Point(19, 139);
+            this.subPanel.Location = new System.Drawing.Point(19, 125);
             this.subPanel.Name = "subPanel";
             this.subPanel.Size = new System.Drawing.Size(231, 66);
             this.subPanel.TabIndex = 9;
@@ -200,7 +212,7 @@ namespace wolfPack_Assign3
             this.panel1.Controls.Add(this.avgUser);
             this.panel1.Controls.Add(this.highUser);
             this.panel1.Controls.Add(this.lowUser);
-            this.panel1.Location = new System.Drawing.Point(19, 282);
+            this.panel1.Location = new System.Drawing.Point(19, 244);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(231, 66);
             this.panel1.TabIndex = 10;
@@ -241,7 +253,7 @@ namespace wolfPack_Assign3
             // silverAward
             // 
             this.silverAward.AutoSize = true;
-            this.silverAward.Location = new System.Drawing.Point(19, 411);
+            this.silverAward.Location = new System.Drawing.Point(19, 381);
             this.silverAward.Name = "silverAward";
             this.silverAward.Size = new System.Drawing.Size(56, 19);
             this.silverAward.TabIndex = 11;
@@ -251,7 +263,7 @@ namespace wolfPack_Assign3
             // goldAward
             // 
             this.goldAward.AutoSize = true;
-            this.goldAward.Location = new System.Drawing.Point(19, 436);
+            this.goldAward.Location = new System.Drawing.Point(19, 406);
             this.goldAward.Name = "goldAward";
             this.goldAward.Size = new System.Drawing.Size(52, 19);
             this.goldAward.TabIndex = 12;
@@ -261,7 +273,7 @@ namespace wolfPack_Assign3
             // platAward
             // 
             this.platAward.AutoSize = true;
-            this.platAward.Location = new System.Drawing.Point(19, 461);
+            this.platAward.Location = new System.Drawing.Point(19, 431);
             this.platAward.Name = "platAward";
             this.platAward.Size = new System.Drawing.Size(75, 19);
             this.platAward.TabIndex = 13;
@@ -273,7 +285,7 @@ namespace wolfPack_Assign3
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label8.Location = new System.Drawing.Point(110, 411);
+            this.label8.Location = new System.Drawing.Point(110, 381);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(75, 19);
             this.label8.TabIndex = 14;
@@ -284,7 +296,7 @@ namespace wolfPack_Assign3
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.150944F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label9.Location = new System.Drawing.Point(15, 532);
+            this.label9.Location = new System.Drawing.Point(15, 522);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(39, 19);
             this.label9.TabIndex = 15;
@@ -293,7 +305,7 @@ namespace wolfPack_Assign3
             // subComboBox
             // 
             this.subComboBox.FormattingEnabled = true;
-            this.subComboBox.Location = new System.Drawing.Point(119, 445);
+            this.subComboBox.Location = new System.Drawing.Point(114, 408);
             this.subComboBox.Name = "subComboBox";
             this.subComboBox.Size = new System.Drawing.Size(204, 21);
             this.subComboBox.TabIndex = 16;
@@ -301,16 +313,123 @@ namespace wolfPack_Assign3
             // userComboBox
             // 
             this.userComboBox.FormattingEnabled = true;
-            this.userComboBox.Location = new System.Drawing.Point(19, 554);
+            this.userComboBox.Location = new System.Drawing.Point(19, 544);
             this.userComboBox.Name = "userComboBox";
             this.userComboBox.Size = new System.Drawing.Size(304, 21);
             this.userComboBox.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.greaterThanRadioButton);
+            this.panel2.Controls.Add(this.lessThanRadioButton);
+            this.panel2.Location = new System.Drawing.Point(19, 647);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(179, 70);
+            this.panel2.TabIndex = 18;
+            // 
+            // lessThanRadioButton
+            // 
+            this.lessThanRadioButton.AutoSize = true;
+            this.lessThanRadioButton.Location = new System.Drawing.Point(8, 8);
+            this.lessThanRadioButton.Name = "lessThanRadioButton";
+            this.lessThanRadioButton.Size = new System.Drawing.Size(148, 19);
+            this.lessThanRadioButton.TabIndex = 0;
+            this.lessThanRadioButton.TabStop = true;
+            this.lessThanRadioButton.Text = "Less Than or Equal To";
+            this.lessThanRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // greaterThanRadioButton
+            // 
+            this.greaterThanRadioButton.AutoSize = true;
+            this.greaterThanRadioButton.Location = new System.Drawing.Point(8, 43);
+            this.greaterThanRadioButton.Name = "greaterThanRadioButton";
+            this.greaterThanRadioButton.Size = new System.Drawing.Size(163, 19);
+            this.greaterThanRadioButton.TabIndex = 1;
+            this.greaterThanRadioButton.TabStop = true;
+            this.greaterThanRadioButton.Text = "Greater Than or Equal To";
+            this.greaterThanRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(204, 664);
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown.TabIndex = 19;
+            // 
+            // dateQuery
+            // 
+            this.dateQuery.ForeColor = System.Drawing.Color.Red;
+            this.dateQuery.Location = new System.Drawing.Point(337, 39);
+            this.dateQuery.Name = "dateQuery";
+            this.dateQuery.Size = new System.Drawing.Size(75, 38);
+            this.dateQuery.TabIndex = 20;
+            this.dateQuery.Text = "Query";
+            this.dateQuery.UseVisualStyleBackColor = true;
+            // 
+            // subQuery
+            // 
+            this.subQuery.ForeColor = System.Drawing.Color.Red;
+            this.subQuery.Location = new System.Drawing.Point(337, 137);
+            this.subQuery.Name = "subQuery";
+            this.subQuery.Size = new System.Drawing.Size(75, 38);
+            this.subQuery.TabIndex = 21;
+            this.subQuery.Text = "Query";
+            this.subQuery.UseVisualStyleBackColor = true;
+            // 
+            // userQuery
+            // 
+            this.userQuery.ForeColor = System.Drawing.Color.Red;
+            this.userQuery.Location = new System.Drawing.Point(337, 268);
+            this.userQuery.Name = "userQuery";
+            this.userQuery.Size = new System.Drawing.Size(75, 38);
+            this.userQuery.TabIndex = 22;
+            this.userQuery.Text = "Query";
+            this.userQuery.UseVisualStyleBackColor = true;
+            // 
+            // awardQuery
+            // 
+            this.awardQuery.ForeColor = System.Drawing.Color.Red;
+            this.awardQuery.Location = new System.Drawing.Point(337, 398);
+            this.awardQuery.Name = "awardQuery";
+            this.awardQuery.Size = new System.Drawing.Size(75, 38);
+            this.awardQuery.TabIndex = 23;
+            this.awardQuery.Text = "Query";
+            this.awardQuery.UseVisualStyleBackColor = true;
+            // 
+            // userPostQuery
+            // 
+            this.userPostQuery.ForeColor = System.Drawing.Color.Red;
+            this.userPostQuery.Location = new System.Drawing.Point(337, 522);
+            this.userPostQuery.Name = "userPostQuery";
+            this.userPostQuery.Size = new System.Drawing.Size(75, 38);
+            this.userPostQuery.TabIndex = 24;
+            this.userPostQuery.Text = "Query";
+            this.userPostQuery.UseVisualStyleBackColor = true;
+            // 
+            // thresholdQuery
+            // 
+            this.thresholdQuery.ForeColor = System.Drawing.Color.Red;
+            this.thresholdQuery.Location = new System.Drawing.Point(337, 656);
+            this.thresholdQuery.Name = "thresholdQuery";
+            this.thresholdQuery.Size = new System.Drawing.Size(75, 38);
+            this.thresholdQuery.TabIndex = 25;
+            this.thresholdQuery.Text = "Query";
+            this.thresholdQuery.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(956, 719);
+            this.ClientSize = new System.Drawing.Size(1036, 739);
+            this.Controls.Add(this.thresholdQuery);
+            this.Controls.Add(this.userPostQuery);
+            this.Controls.Add(this.awardQuery);
+            this.Controls.Add(this.userQuery);
+            this.Controls.Add(this.subQuery);
+            this.Controls.Add(this.dateQuery);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.userComboBox);
             this.Controls.Add(this.subComboBox);
             this.Controls.Add(this.label9);
@@ -335,6 +454,9 @@ namespace wolfPack_Assign3
             this.subPanel.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -366,6 +488,16 @@ namespace wolfPack_Assign3
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.ComboBox subComboBox;
         private System.Windows.Forms.ComboBox userComboBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton greaterThanRadioButton;
+        private System.Windows.Forms.RadioButton lessThanRadioButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.Button dateQuery;
+        private System.Windows.Forms.Button subQuery;
+        private System.Windows.Forms.Button userQuery;
+        private System.Windows.Forms.Button awardQuery;
+        private System.Windows.Forms.Button userPostQuery;
+        private System.Windows.Forms.Button thresholdQuery;
     }
 }
 
